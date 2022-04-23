a3375f0c34049a472e35e038925d3db74d21842f
VeraJ99 <90844547+VeraJ99@users.noreply.github.com>
2022-04-23 17:49:50 +0300

    Add files via upload

---
+++ Homework5/Main.java
@@ -0,0 +1,19 @@
+package Homework5;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        AppData appData = new AppData();
+        appData.load("table.csv");
+
+        System.out.println(Arrays.toString(appData.getHeader()));
+        System.out.println(Arrays.deepToString(appData.getData()));
+
+        appData.save("table2. csv");
+    }
+}
+
+
